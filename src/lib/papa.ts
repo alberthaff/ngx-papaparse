e58e1e373a9c4b8112b96b29b88df5eec1954911
Albert Haff <ahn@haffdata.com>
2018-09-14 16:10:22 +0200

    - Renamed papaLib to _papa in service - Added a few more unit tests

--- src/lib/papa.ts
+++ src/lib/papa.ts
@@ -7,7 +7,7 @@ import * as lib from 'papaparse/papaparse.min.js';
 
 @Injectable()
 export class Papa {
-    private papaLib = lib;
+    public _papa = lib;
 
     constructor(@Optional() @Inject('PapaParseGlobalConfig') private config?: PapaParseGlobalConfig
     ) {
@@ -23,8 +23,7 @@ export class Papa {
         if (config) {
             if (config.worker === true) {
                 if (this.config.scriptPath) {
-                    console.log('CONFIG!', config);
-                    this.papaLib.SCRIPT_PATH = this.config.scriptPath;
+                    this._papa.SCRIPT_PATH = this.config.scriptPath;
                 } else {
                     throw new Error('When using workers, the workerScriptPath must be defined in global' +
                         ' papaparse configuration. See' +
@@ -34,14 +33,14 @@ export class Papa {
             }
         }
 
-        return this.papaLib.parse(csv, config);
+        return this._papa.parse(csv, config);
     }
 
     /**
      * Convert an array into CSV
      */
     public unparse(data, config?: PapaUnparseConfig): string {
-        return this.papaLib.unparse(data, config);
+        return this._papa.unparse(data, config);
     }
 
     /**
@@ -50,7 +49,7 @@ export class Papa {
      * exist on the local computer. Default 10 MB.
      */
     public setLocalChunkSize(value: number): void {
-        this.papaLib.LocalChunkSize = value;
+        this._papa.LocalChunkSize = value;
     }
 
     /**
@@ -58,21 +57,21 @@ export class Papa {
      * Used when streaming remote files. Default 5 MB.
      */
     public setRemoteChunkSize(value: number): void {
-        this.papaLib.RemoteChunkSize = value;
+        this._papa.RemoteChunkSize = value;
     }
 
     /**
      * Set the delimiter used when it is left unspecified and cannot be detected automatically. Default is comma.
      */
     public setDefaultDelimiter(value: string): void {
-        this.papaLib.DefaultDelimiter = value;
+        this._papa.DefaultDelimiter = value;
     }
 
     /**
      * An array of characters that are not allowed as delimiters.
      */
     get badDelimiters() {
-        return this.papaLib.BAD_DELIMITERS;
+        return this._papa.BAD_DELIMITERS;
     }
 
     /**
@@ -80,14 +79,14 @@ export class Papa {
      * Should be doing the job we strangely rely upon commas and tabs for.
      */
     get recordSeperator() {
-        return this.papaLib.RECORD_SEP;
+        return this._papa.RECORD_SEP;
     }
 
     /**
      * Also sometimes used as a delimiting character. ASCII code 31.
      */
     get unitSeperator() {
-        return this.papaLib.UNIT_SEP;
+        return this._papa.UNIT_SEP;
     }
 
     /**
@@ -95,6 +94,6 @@ export class Papa {
      * If false, worker: true will have no effect.
      */
     get workersSupported(): boolean {
-        return this.papaLib.WORKERS_SUPPORTED;
+        return this._papa.WORKERS_SUPPORTED;
     }
 }
