a73051f4eb50cd7553fb95fbabf1cfec5777d3b2
Albert Haff <ahn@haffdata.com>
2019-06-27 00:28:21 +0200

    Removed outdated test

--- src/lib/papa.spec.ts
+++ src/lib/papa.spec.ts
@@ -39,17 +39,6 @@ describe('Papa', () => {
         // TODO
     }));
 
-    it('should throw exception if trying to use workers without scriptPath defined', inject([Papa], (papa: Papa) => {
-        const csv = '"a","b,","c"""\nd,e,f\ng,h,i';
-
-        expect(() => papa.parse(csv, {
-            worker: true
-        })).toThrow(Error('When using workers, the workerScriptPath must be defined in global' +
-            ' papaparse configuration. See' +
-            ' https://alberthaff.dk/projects/ngx-papaparse/docs/v3/parsing-csv/using-serviceworkers' +
-            ' for more information.'));
-    }));
-
     it('should generate CSV from array', inject([Papa], (papa: Papa) => {
         const data = [
             ['a', 'b,', 'c"'],
