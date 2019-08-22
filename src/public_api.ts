f129a3d79a0b8d5885d9a235bf16c6424e1bc802
Koichi Hatano <kohatang@gmail.com>
2019-08-22 16:27:50 +0900

    fix: add export parse-error

--- src/public_api.ts
+++ src/public_api.ts
@@ -8,3 +8,4 @@ export * from './lib/interfaces/parse-meta';
 export * from './lib/interfaces/papa-parse-parser';
 export * from './lib/interfaces/parse-result';
 export * from './lib/interfaces/unparse-config';
+export * from './lib/interfaces/parse-error';
